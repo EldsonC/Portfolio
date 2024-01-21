2a592b7cbc03887cd85dff14f0e60cf953568e5d
Einstein <eldson.caldasw@gmail.com>
2024-01-21 12:24:49 -0300

    api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://notify-flame.vercel.app/",
-    // baseURL: "http://localhost:3002/",
-    // headers: {
-    //     // Adicione aqui os cabeçalhos personalizados, se necessário
-    //     "Content-Type": "application/json",
-    //     // Outros cabeçalhos, se necessário
-    //   },
-})
+  baseURL: "https://notify-flame.vercel.app/",
+});
